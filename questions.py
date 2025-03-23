d41a471a5ebdf244424dbe1aa2c70c3231be4993
Noemi Gudinio <noeamaliagudino@gmail.com>
2025-03-23 17:54:49 -0300

    Agregada funcionalidad para no acceder a las 3 listas usando índices.

--- questions.py
+++ questions.py
@@ -29,7 +29,7 @@ correct_answers_index = [1, 2, 0, 3, 1]
 # Inicialización del puntaje
 puntaje = 0
 
-# El usuario deberá contestar 3 preguntas
+'''# El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
     question_index = random.randint(0, len(questions) - 1)
@@ -37,6 +37,19 @@ for _ in range(3):
     # Se muestra la pregunta y las respuestas posibles
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
+        print(f"{i + 1}. {answer}")'''
+        
+ # Se combina las preguntas, respuestas y respuestas correctas en una lista
+combined_questions = list(zip(questions, answers, correct_answers_index))
+
+# Se seleccionan 3 preguntas aleatorias de la lista de preguntas
+questions_to_ask = random.choices(combined_questions, k=3)
+
+# El usuario deberá contestar 3 preguntas
+for question, possible_answers, correct_index in questions_to_ask:
+    # Se muestra la pregunta y las respuestas posibles
+    print(question)
+    for i, answer in enumerate(possible_answers): 
         print(f"{i + 1}. {answer}")
         
     # Variable para rastrear si el usuario acertó
@@ -57,7 +70,7 @@ for _ in range(3):
         user_answer = int(user_input) - 1
 
         # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+        if user_answer == correct_index:
            print("¡Correcto!")
            puntaje = puntaje + 1
            acierto = True  
@@ -70,7 +83,7 @@ for _ in range(3):
         # se muestra la respuesta correcta
     if not acierto:
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(possible_answers[correct_index])
         puntaje = puntaje - 0.5
     # Se imprime un blanco al final de la pregunta
     print()
