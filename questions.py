14fd2a50b8c61e8a97fea596e41ce028e3cce802
Noemi Gudinio <noeamaliagudino@gmail.com>
2025-03-23 17:19:23 -0300

    Agregada la funcionalidad para validación de respuestas

--- questions.py
+++ questions.py
@@ -8,6 +8,7 @@ questions = [
     "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
     "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",
 ]
+
 # Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
 answers = [
     ("size()", "len()", "length()", "count()"),
@@ -21,7 +22,8 @@ answers = [
     ),
     ("=", "==", "!=", "==="),
 ]
-# Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
+
+# Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
 # El usuario deberá contestar 3 preguntas
@@ -33,17 +35,36 @@ for _ in range(3):
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
         print(f"{i + 1}. {answer}")
+        
+    # Variable para rastrear si el usuario acertó
+    acierto = False
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
+        # Se solicita la respuesta del usuario
+        user_input = input("Respuesta: ")
+
+       # Verificar si la entrada es un número dentro del rango válido
+        if not user_input.isdigit() or not (1 <= int(user_input) <= 4):
+            print("Respuesta no válida. Debe ser un número entre 1 y 4.")
+            print("Fin del juego.")
+            exit(1)
+        
+        # Convertir la respuesta a un número entero entre 0 y 3
+        user_answer = int(user_input) - 1
+
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
-            print("¡Correcto!")
-            break
-    else:
+           print("¡Correcto!")
+           acierto = True  
+           break  
+        elif intento < 1: # Mostrar "Intenta de nuevo" solo si no es el último intento
+               print("Incorrecto. Intenta de nuevo.")
+            
+    
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
+    if not acierto:
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
 
